21187512b85fa24c6fcf092e44da9fbb02c4f203
nakulupadhyay <142798383+nakulupadhyay@users.noreply.github.com>
2025-08-21 11:38:21 +0530

    Add files via upload

---
+++ practice_problem/prg8.java
@@ -0,0 +1,16 @@
+package bridge_Labz;
+import java.util.*;
+public class prg8 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int base=sc.nextInt();
+		int exponent=sc.nextInt();
+		double ans=Math.pow(base,exponent);
+		System.out.println(ans);
+		
+		
+
+	}
+
+}
---
+++ practice_problem/prg9.java
@@ -0,0 +1,19 @@
+package bridge_Labz;
+import java.util.*;
+
+public class prg9 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter three numbers");
+		int a= sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int avr=(a*b*c)/3;
+		System.out.println("Average:"+avr);
+		
+		
+
+	}
+
+}
